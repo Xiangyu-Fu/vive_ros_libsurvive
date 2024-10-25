cc955327c34c4e6dc36d47c2a171d1c8c2798ef5
stan <stan_fu@163.com>
2024-10-25 23:13:19 +0200

    update ros node using api survive

--- src/applications/libsurvive_pub_node.cpp
+++ src/applications/libsurvive_pub_node.cpp
@@ -1,48 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+#include <survive_api.h>
+#include <os_generic.h>
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <std_msgs/Bool.h>
-#include <survive.h>
+#include <std_msgs/String.h>
 
+// Keep the node running
 static volatile int keepRunning = 1;
 
-void button_process(SurviveObject *so, enum SurviveInputEvent eventType, enum SurviveButton buttonId,
-                    const enum SurviveAxis *axisIds, const SurviveAxisVal_t *axisVals) {
-    ROS_INFO("Button event on object: %s, Button ID: %d", so->serial_number, buttonId);
+#ifdef __linux__
+#include <assert.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void intHandler(int dummy) {
+    if (keepRunning == 0)
+        exit(-1);
+    keepRunning = 0;
+}
+#endif
+
+// Log function for Survive API
+static void log_fn(SurviveSimpleContext *actx, SurviveLogLevel logLevel, const char *msg) {
+    fprintf(stderr, "(%7.3f) SimpleApi: %s\n", survive_simple_run_time(actx), msg);
 }
 
+// Main function for ROS node
 int main(int argc, char **argv) {
-    // ROS initialization
-    ros::init(argc, argv, "libsurvive_pub_node");
-    ROS_INFO("Starting Libsurvive publisher node...");
-    ros::NodeHandle nh;
-    
-    // ROS publishers
-    ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("survive/pose", 10);
-    ros::Publisher button_pub = nh.advertise<std_msgs::Bool>("survive/button", 10);
-    ROS_INFO("Publishing pose and button events...");
+#ifdef __linux__
+    signal(SIGINT, intHandler);
+    signal(SIGTERM, intHandler);
+    signal(SIGKILL, intHandler);
+#endif
 
-    // Libsurvive initialization
-    SurviveContext *ctx = survive_init(argc, argv);
-    if (!ctx) {
-        ROS_ERROR("Failed to initialize Libsurvive.");
+    // Initialize ROS
+    ros::init(argc, argv, "survive_pub_node");
+    ros::NodeHandle nh;
+
+    // Publishers for Pose and Button Events
+    ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("survive/pose", 10);
+    ros::Publisher button_pub = nh.advertise<std_msgs::String>("survive/button_event", 10);
+
+    // Initialize libsurvive with logging
+    SurviveSimpleContext *actx = survive_simple_init_with_logger(argc, argv, log_fn);
+    if (actx == 0) {
+        ROS_ERROR("Failed to initialize libsurvive.");
         return 1;
     }
 
-    survive_startup(ctx);
-    survive_install_button_fn(ctx, button_process);
+    // Start Survive's tracking in a separate thread
+    survive_simple_start_thread(actx);
 
-    // Main loop: poll for tracking data
-    ros::Rate loop_rate(30);  // 30 Hz loop
-    while (ros::ok() && survive_poll(ctx) == 0) {
-        // In a real application, you would retrieve pose data from SurviveObject and publish it here
-        geometry_msgs::PoseStamped pose_msg;
-        // Fill in pose_msg from SurviveObject
-        pose_pub.publish(pose_msg);
+    ROS_INFO("Starting libsurvive tracking...");
+
+    struct SurviveSimpleEvent event = {};
+    while (ros::ok() && keepRunning && survive_simple_wait_for_event(actx, &event) != SurviveSimpleEventType_Shutdown) {
+        switch (event.event_type) {
+            case SurviveSimpleEventType_PoseUpdateEvent: {
+                const struct SurviveSimplePoseUpdatedEvent *pose_event = survive_simple_get_pose_updated_event(&event);
+                SurvivePose pose = pose_event->pose;
+
+                // Publish Pose
+                geometry_msgs::PoseStamped pose_msg;
+                pose_msg.header.stamp = ros::Time::now();
+                pose_msg.pose.position.x = pose.Pos[0];
+                pose_msg.pose.position.y = pose.Pos[1];
+                pose_msg.pose.position.z = pose.Pos[2];
+                pose_msg.pose.orientation.x = pose.Rot[0];
+                pose_msg.pose.orientation.y = pose.Rot[1];
+                pose_msg.pose.orientation.z = pose.Rot[2];
+                pose_msg.pose.orientation.w = pose.Rot[3];
+
+                pose_pub.publish(pose_msg);
+                break;
+            }
+
+            case SurviveSimpleEventType_ButtonEvent: {
+                const struct SurviveSimpleButtonEvent *button_event = survive_simple_get_button_event(&event);
+                SurviveObjectSubtype subtype = survive_simple_object_get_subtype(button_event->object);
+
+                // Publish Button Event as String
+                std_msgs::String button_msg;
+                button_msg.data = "Object " + std::string(survive_simple_object_name(button_event->object)) +
+                                  " Button " + SurviveButtonsStr(subtype, button_event->button_id) +
+                                  " Event " + SurviveInputEventStr(button_event->event_type);
+
+                button_pub.publish(button_msg);
+                break;
+            }
+
+            case SurviveSimpleEventType_ConfigEvent: {
+                const struct SurviveSimpleConfigEvent *cfg_event = survive_simple_get_config_event(&event);
+                ROS_INFO("(%f) %s received configuration of length %u", cfg_event->time,
+                         survive_simple_object_name(cfg_event->object), (unsigned)strlen(cfg_event->cfg));
+                break;
+            }
+
+            case SurviveSimpleEventType_DeviceAdded: {
+                const struct SurviveSimpleObjectEvent *obj_event = survive_simple_get_object_event(&event);
+                ROS_INFO("(%f) Found '%s'", obj_event->time, survive_simple_object_name(obj_event->object));
+                break;
+            }
+
+            case SurviveSimpleEventType_None:
+                break;
+        }
 
         ros::spinOnce();
-        loop_rate.sleep();
     }
 
-    survive_close(ctx);
+    ROS_INFO("Shutting down libsurvive...");
+    survive_simple_close(actx);
     return 0;
 }
